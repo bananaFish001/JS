aced24e6e35307b2690af17b1096229fc3be47eb
bananaFish001 <raj123.rs308@gmail.com>
2024-02-25 01:55:09 +0530

    Some insights in type conversion

---
+++ 03_conversionOperations.js
@@ -0,0 +1,25 @@
+let score = NaN
+
+console.log(typeof score)
+console.log((typeof(score)))
+
+let valueInNumber = Number(score)
+console.log(valueInNumber)
+console.log(typeof(valueInNumber))
+
+// Type Conversion:
+// "33" to number equals 33
+// "33abc" to number equals NaN
+// "true" to number equals 1 and false to number equals 0
+
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn)
+
+let isHere = ""
+let booleanIsHere = Boolean(isHere)
+
+console.log(booleanIsHere)
+
