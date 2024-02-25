fda963da5994365463a0964d4b43a19fa4dac3bf
bananaFish001 <raj123.rs308@gmail.com>
2024-02-25 23:04:53 +0530

    Some fun with comparion operators

--- 03_conversionOperations.js
+++ 03_conversionOperations.js
@@ -23,3 +23,40 @@ let booleanIsHere = Boolean(isHere)
 
 console.log(booleanIsHere)
 
+
+// ***********OPERATIONS***********
+
+let value = 3
+let negValue = -value
+
+console.log(negValue)
+
+
+console.log(2+2)
+console.log(2-2)
+console.log(2**3)
+console.log(2*3)
+console.log(2%3)
+console.log(2/3)
+
+
+
+// String OPERATIONS
+
+let str1 = "Raj"
+let str2 = "Singh"
+let str3 = str1 + str2
+console.log(str3)
+
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 3)
+console.log(1 + 2 + "3")
+
+// prefix and postfix
+
+let x = 3
+const y = x++
+console.log(`x: ${x}, y: ${y}`)
+
