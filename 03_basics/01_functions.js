c57f3196c8500060c964cc1825a132e3ce6d8a39
bananaFish001 <raj123.rs308@gmail.com>
2024-03-04 23:26:10 +0530

    More functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -13,10 +13,10 @@ function addNums(num1, num2){
   //console.log(num1 + num2)
   let result = num1 + num2
   return result
-  console.log("Hello")
+  // console.log("Hello")
 }
 
-const result = addNums(3,4);
+//const result = addNums(3,4);
 // console.log(result) // It will print undefined cuz function is not returning anything
 
 function userLogin(user ="Sammy"){ // Sammy here is a default value
@@ -27,5 +27,35 @@ function userLogin(user ="Sammy"){ // Sammy here is a default value
   return `${user} just logged in!`
 }
 
-console.log(userLogin()) // If you don't pass in any value it will return undefined
+// console.log(userLogin()) // If you don't pass in any value it will return undefined
 
+
+function calculateCartPrice(...num1){ // 
+  return num1
+}
+
+// console.log(calculateCartPrice(200, 300, 400, 500));
+
+const userName = {
+  username : "Kyuren",
+  price: "free"
+}
+
+function userHandle(anyobject){
+  console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`)
+}
+
+// userHandle(userName);
+userHandle({
+  username: "PulPul",
+  price: "Still Free"
+})
+
+const newArray = [200, 300, 600]
+
+function returnArray(getArray){
+  return getArray[2]
+}
+
+// console.log(returnArray(newArray))
+console.log(returnArray([100, 400, 800]))
---
+++ 03_basics/function_testing.js
@@ -0,0 +1,10 @@
+const user = {
+  userName: "Tina",
+  email: "t@gmail.com"
+}
+
+function userHandle(anyobject){
+  console.log(`Username is ${anyobject.userName}`)
+}
+
+userHandle(user);
