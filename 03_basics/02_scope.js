a121c969bb4e8c8b4154738c1fb0a9703019ebef
bananaFish001 <raj123.rs308@gmail.com>
2024-03-04 23:58:40 +0530

    Some more scopes in function

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -4,9 +4,45 @@ if(true){
   let a = 10
   const b = 20
   var c = 30
-  console.log(`Inner a: ${a}`)
+  //console.log(`Inner a: ${a}`)
 }
 
-console.log(a)
+//console.log(a)
 // console.log(b)
 // console.log(c) // only c will give output cuz var can be accessed out of scope
+
+
+// Nested scope
+function one(){
+  const userName = "kyuren"
+
+
+  function two(){ // This function can access the values from it's parent function but not vice versa
+    const website = "Uthube"
+    console.log(userName) 
+  }
+  //console.log(website)
+
+  two()
+}
+
+one()
+
+if(true){
+  const newUser = "PulPul"
+  if(newUser === "PulPul"){
+    const website = "only fans"
+    console.log(newUser +" " + website)
+  }
+}
+// ++++++++++++++++++++++++++++++++Interesting++++++++++++++++++++++++++++++++++++
+
+console.log(addOne(5))
+function addOne(num){
+  return num + 1
+}
+
+addTwo(5) // Cannot be accessed by addTwo function
+function addTwo(num){
+  return num + 2
+}
