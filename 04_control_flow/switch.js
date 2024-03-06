e7880b6fb7d4e5401c80790f02e8ded8a594649a
bananaFish001 <raj123.rs308@gmail.com>
2024-03-06 22:36:24 +0530

    Learned about switch statements

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,19 @@
+const month = 4
+
+switch(month){
+  case 1:
+        console.log("January");
+        break;
+  case 2:
+        console.log("February");
+        break;
+  case 3:
+        console.log("March");
+        break;
+  case 4:
+        console.log("April");
+        break;
+  default:
+        console.log("Entry does not match")
+        break;
+}
