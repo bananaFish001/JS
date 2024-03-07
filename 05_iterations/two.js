2ad2d9fbbbcec39fb985a99c10e12955011dc2a3
bananaFish001 <raj123.rs308@gmail.com>
2024-03-07 20:46:04 +0530

    Learning about whiel loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,21 @@
+let index = 0
+
+while(index <= 10){
+  console.log(`Value of index is ${index}`);
+  index += 2
+}
+//console.log(`Index is ${index}`)
+
+let myArray = ["Thorfinn", "Denji", "Guts"]
+let arr = 0
+while(arr < myArray.length){
+  console.log(`Best Characters are ${myArray[arr]}`);
+  arr += 1;
+}
+
+let score = 11
+
+do{
+  console.log(`Score is ${score}`);
+  score += 1
+} while(score <= 10)
